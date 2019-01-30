3fe71525a5ec100972b30a87ac8ebc2307ce1fce
aizf <>
2019-01-31 01:31:18 +0800

    1

--- craw.py
+++ craw.py
@@ -1,12 +1,10 @@
 import io
 import json
 import random
-import re
 import sys
 import time
+import xlwt
 
-import pymysql
-import redis
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -18,105 +16,112 @@ sys.stdout = io.TextIOWrapper(
 
 
 class Craw:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, keyword):
         self.__browser = browser
         self.__url = url
         browser.get(self.__url)  # 工作页面
-        self.keyWord="万科"
+        self.keyword = keyword
 
     def __prepare(self):
-        self.__browser.find_element_by_id("code_suggest").send_keys(self.keyWord,Keys.ENTER)
-        # print("handles ",str(self.__browser.window_handles))
-        # print(self.__browser.title)
-        self.__browser.switch_to.window(self.__browser.window_handles[-1])
-        # print(self.__browser.title)
-        self.__browser.find_element_by_css_selector(
-            "div.module.module-share-list > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > a"
-            ).click()
-        self.__browser.switch_to.window(self.__browser.window_handles[-1])
-        self.__browser.find_element_by_css_selector(
-            "body > div:nth-child(13) > div.hqrls > div:nth-child(1) > a:nth-child(9)"
-            ).click()
-        self.__browser.switch_to.window(self.__browser.window_handles[-1])
-
-    def crawlCnRank_gen(self):
         browser = self.__browser
-        db = pymysql.connect(
-            host="localhost",
-            user="root",
-            password="123456",
-            database="cnrank",
-            charset="utf8")
-        cursor = db.cursor()
-        table = browser.find_element_by_css_selector(
-            "body > div.maxtopbar > div.main-shadow-box > div.main-container > div.container.xuning-box > div:nth-child(2)"
-        ).find_elements_by_tag_name("table")
-        anonym = 0
-        pattern1 = re.compile(r"detail/(\w+)")
-        pattern2 = re.compile(r"hphover/(\w+)_hphover.png")
-        for i in range(1, 11):
-            if i < 6:
-                camp = "radiant"
-                tableNum = 0
-            else:
-                camp = "dire"
-                tableNum = 1
-                i -= 5
-            ggamer = table[tableNum].find_element_by_css_selector(
-                "tbody > tr:nth-child(" + str(i) +
-                ") > td:nth-child(2) > a").text
-            if "匿名玩家" in ggamer:
-                gamer = 'anonym' + str(anonym)
-                gamerId = 'none' + str(anonym)
-                anonym += 1
-            else:
-                gamer = ggamer
-                gamerId = pattern1.findall(
-                    table[tableNum].find_element_by_css_selector(
-                        "tbody > tr:nth-child(" + str(i) +
-                        ") > td:nth-child(2) > a").get_attribute('href'))[0]
-                #                                      3    4      5        6        7       8       9        10          11          12    13
-            sql = """INSERT IGNORE INTO GEN(Id, camp, gamerid, gamer, IdAndGamer, hero, KDA, frate, perdamage, damage, lastHit, expMin , goldMin, buildingDamage, heal, goods)
-                        VALUES ("""+"\'"+self.__Id+"\'"+','+"\'"+camp+"\'"+',' +\
-                "\'"+gamerId+"\'"+',' +\
-                "\'"+gamer+"\'"+',' +\
-                "\'"+self.__Id+"+"+gamerId+"\'"+',' +\
-                "\'"+pattern2.findall(table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(3) > a > img").get_attribute('src'))[0]+"\'"+',' +\
-                "\'"+table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(4)").text+"\'"+',' +\
-                "\'"+table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(5)").text+"\'"+',' +\
-                "\'"+table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(6)").text+"\'"+',' +\
-                "\'"+table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(7)").text+"\'"+',' +\
-                "\'"+table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(8)").text+"\'"+',' +\
-                "\'"+table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(9)").text+"\'"+',' +\
-                "\'"+table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(10)").text+"\'"+',' +\
-                "\'"+table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(11)").text+"\'"+',' +\
-                "\'"+table[tableNum].find_element_by_css_selector("tbody > tr:nth-child("+str(i)+") > td:nth-child(12)").text+"\'"+',' +\
-                "\'"+self.__eqListToJson(table[tableNum].find_element_by_css_selector(
-                    "tbody > tr:nth-child("+str(i)+") > td:nth-child(13)"))+"\'"+")"
-            # print(sql)
-            try:
-                cursor.execute(sql)  # 执行sql语句
-                db.commit()  # 提交到数据库执行
-            except:
-                db.rollback()  # 如果发生错误则回滚
-        db.close()  # 关闭数据库连接
+        WebDriverWait(browser, 10).until(
+            lambda browser: browser.find_element_by_id("code_suggest"))
+        browser.find_element_by_id("code_suggest").send_keys(
+            self.keyword, Keys.ENTER)
+        # print("handles ",str(browser.window_handles))
+        # print(browser.title)
+        browser.switch_to.window(browser.window_handles[-1])
+        # print(browser.title)
+        WebDriverWait(browser, 20).until(
+            lambda browser: browser.find_element_by_css_selector("div.module.module-share-list > div > table > tbody > tr:nth-child(1)")
+        )
+        browser.find_element_by_css_selector(
+            "div.module.module-share-list > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > a"
+        ).click()
+        browser.switch_to.window(browser.window_handles[-1])
+        WebDriverWait(browser, 10).until(
+            lambda browser: browser.find_element_by_css_selector("body > div:nth-child(13) > div.hqrls > div:nth-child(1) > a:nth-child(9)")
+        )  #等财务分析
+        browser.find_element_by_css_selector(
+            "body > div:nth-child(13) > div.hqrls > div:nth-child(1) > a:nth-child(9)"
+        ).click()  #点财务分析
+        browser.switch_to.window(browser.window_handles[-1])
+
+    def crawTheBalanceSheet(self):
+        browser = self.__browser
+        excel = xlwt.Workbook(encoding='utf-8', style_compression=0)
+
+        sheet1 = excel.add_sheet('reportPeriod ', cell_overwrite_ok=True)
+        page = 0
+        #第一格
+        WebDriverWait(browser, 10).until(
+            lambda browser: browser.find_element_by_css_selector("#report_zcfzb > tbody > tr:nth-child(1) > th:nth-child(1)")
+        )  #等
+        sheet1.write(
+            0, 0,
+            browser.find_element_by_css_selector(
+                "#report_zcfzb > tbody > tr:nth-child(1) > th:nth-child(1)").
+            text)
+
+        browser.execute_script(r'$("#report_zcfzb > tbody > tr").show()')
+        trs = browser.find_elements_by_css_selector(
+            "#report_zcfzb > tbody > tr")
+        #第一列[1:]
+        for i, tr in enumerate(trs[1:], 1):
+            head = tr.find_element_by_tag_name("td")
+            sheet1.write(i, 0, head.text)
+
+        while page<100:
+            trs = browser.find_elements_by_css_selector(
+                "#report_zcfzb > tbody > tr")
+            judgeAjax = browser.find_element_by_css_selector(
+                "#report_zcfzb > tbody > tr:nth-child(1) > th:nth-child(2)"
+            ).text
+            #第一行[1:]
+            line = 1 + page * 5
+            for th in trs[0].find_elements_by_tag_name("th")[1:]:
+                sheet1.write(0, line, th.text)
+                line += 1
+            row = 1
+
+            #其他行[1:]
+            for tr in trs[1:]:
+                line = 1 + page * 5
+                for td in tr.find_elements_by_tag_name("td")[1:]:
+                    sheet1.write(row, line, td.text)
+                    line += 1
+                row += 1
+
+            if "none" in browser.find_element_by_css_selector(
+                    "#zcfzb_next").get_attribute("style"):
+                break
+            WebDriverWait(browser, 10).until(
+                EC.element_to_be_clickable( (By.CSS_SELECTOR,"#zcfzb_next") )
+            )  #等可被点击
+            browser.find_element_by_css_selector("#zcfzb_next").click()
+            WebDriverWait(browser, 10).until(
+                lambda browser: judgeAjax != browser.find_element_by_css_selector("#report_zcfzb > tbody > tr:nth-child(1) > th:nth-child(2)").text
+            )  #等加载
+            browser.execute_script(r'$("#report_zcfzb > tbody > tr").show()')
+            page += 1
+
+        excel.save(r'd:\theBalanceSheet.xls')
 
     def runCraw(self):
         try:
             self.__prepare()
         except:
-            print(self.keyWord + " except")
+            print(self.keyword + " except")
         finally:
             time.sleep(random.uniform(1.11, 4.11))
 
     def __test__(self):
         self.__prepare()
+        self.crawTheBalanceSheet()
         print("test done")
 
 
 browser = webdriver.Chrome()
-wait = WebDriverWait(browser, 10)
-craw = Craw(browser, "http://www.eastmoney.com/")
+craw = Craw(browser, "http://www.eastmoney.com/", "万科")
 # craw.runCraw()
 craw.__test__()
-
